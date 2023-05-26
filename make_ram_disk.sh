e4d4e0a0263596d56e6ef23f5c9b7d3eeed49e47
Joe Burmeister <joe.burmeister@devtank.co.uk>
2023-05-26 21:45:59 +0100

    Latest custom ram disk init.

--- make_ram_disk.sh
+++ make_ram_disk.sh
@@ -18,6 +18,8 @@ mkdir -p $DESTDIR
 mkdir -p "$DESTDIR/proc"
 mkdir -p "$DESTDIR/sys"
 mkdir -p "$DESTDIR/dev"
+mkdir -p "$DESTDIR/conf"
+mkdir -p "$DESTDIR/etc"
 
 mkdir  -p "$DESTDIR/usr/lib"
 mkdir  -p "$DESTDIR/usr/lib64"
@@ -28,6 +30,7 @@ ln -s usr/lib "$DESTDIR/lib"
 ln -s usr/lib64 "$DESTDIR/lib64"
 ln -s usr/bin "$DESTDIR/bin"
 ln -s usr/sbin "$DESTDIR/sbin"
+ln -s /proc/mounts "$DESTDIR/etc/mtab"
 
 cp -v /bin/busybox "$DESTDIR/bin/"
 "$DESTDIR/bin/busybox" --install -s "$DESTDIR/bin/"
@@ -42,29 +45,12 @@ copy_exec /usr/sbin/resize2fs
 copy_exec /usr/bin/lsblk
 
 auto_add_modules net ata ide scsi block
+add_loaded_modules
 
-echo "#! /bin/sh
+force_load ata_piix
+force_load sd_mod
 
-mount -t sysfs -o nodev,noexec,nosuid sysfs /sys
-mount -t proc -o nodev,noexec,nosuid proc /proc
-
-mount -t devtmpfs -o nosuid,mode=0755 udev /dev
-
-# Prepare the /dev directory
-[ ! -h /dev/fd ] && ln -s /proc/self/fd /dev/fd
-[ ! -h /dev/stdin ] && ln -s /proc/self/fd/0 /dev/stdin
-[ ! -h /dev/stdout ] && ln -s /proc/self/fd/1 /dev/stdout
-[ ! -h /dev/stderr ] && ln -s /proc/self/fd/2 /dev/stderr
-
-mkdir /dev/pts
-mount -t devpts -o noexec,nosuid,gid=5,mode=0620 devpts /dev/pts || true
-
-modprobe ata_piix
-modprobe sd_mod
-
-/bin/ash
-poweroff -f
-" > "$DESTDIR/init"
+cp "$CONFDIR/init" "$DESTDIR"
 chmod +x "$DESTDIR/init"
 
 depmod -b "$DESTDIR"
