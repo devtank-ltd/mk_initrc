8ec38f9ccc258e1265968ce36bca933688a67d35
Joe Burmeister <joe.burmeister@devtank.co.uk>
2023-05-29 22:09:31 +0100

    Add kernel selection to QEmu test script.

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
 #https://www.kernel.org/doc/html/latest/admin-guide/initrd.html
 
-qemu-system-x86_64 -machine accel=kvm -m 2G -hda disk.img -serial stdio -append "root=/dev/ram0 rw console=ttyS0" -kernel $(ls /boot/vmlinuz-$(uname -r)) -initrd $1
+if [ -z "$KERNEL" ]
+then
+  KERNEL=$(uname -r)
+fi
+
+qemu-system-x86_64 -machine accel=kvm -m 2G -serial stdio -append "root=/dev/ram0 rw console=tty0" -kernel $(ls /boot/vmlinuz-$KERNEL) -initrd $1
