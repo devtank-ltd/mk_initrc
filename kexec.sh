4efb0dfe4ee8ce87631938c1d834419352f5b268
Joe Burmeister <joe.burmeister@devtank.co.uk>
2023-05-29 20:50:04 +0100

    Fix hard coded kernel.

--- kexec.sh
+++ kexec.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-kexec -l /boot/vmlinuz-5.10.0-23-cloud-amd64 --initrd=$1 --command-line="root=/dev/ram0 rw console=tty0 console=ttyS0,115200 earlyprintk=ttyS0,115200"
+kexec -l /boot/vmlinuz-$(uname -r) --initrd=$1 --command-line="root=/dev/ram0 rw console=tty0 console=ttyS0,115200 earlyprintk=ttyS0,115200"
 kexec -e
